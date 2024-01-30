89a855aaf68129a1c0420678f511b8e97b1821b5
sanalkumars <sanalskumar126@gmail.com>
2024-01-30 22:04:40 +0530

    commit after finishing header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai'
 import {FaMoon} from 'react-icons/fa'
 
@@ -7,6 +7,9 @@ import {FaMoon} from 'react-icons/fa'
 
 
 const Header = () => {
+
+  const path = useLocation().pathname;
+
   return (
     <Navbar className='border-b-2' >
       <Link to={"/"}  className='self-center whitespace-nowrap text-sm
@@ -36,14 +39,25 @@ const Header = () => {
                 Sign In
               </Button>
           </Link>
+          <Navbar.Toggle />
+        </div>
           <Navbar.Collapse>
-            <Navbar.Link active>
+            <Navbar.Link active={path==="/"} as={'div'} >
               <Link to='/' >
                 Home
               </Link>
             </Navbar.Link>
+            <Navbar.Link active={path==="/about"} as={'div'}>
+              <Link to='/about' >
+                About
+              </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path==="/project"} as={'div'}>
+              <Link to='/project' >
+                Projects
+              </Link>
+            </Navbar.Link>
           </Navbar.Collapse>
-        </div>
     </Navbar>
   )
 }
