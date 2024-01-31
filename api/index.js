39b8541afe4b5bca3f5689a48da6ec023ac761ba
sanalkumars <sanalskumar126@gmail.com>
2024-01-31 10:47:54 +0530

    added middleware for error handling

--- api/index.js
+++ api/index.js
@@ -6,23 +6,25 @@ import authRoutes from './routes/authRoute.js'
 dotenv.config();
 
 
-mongoose.connect(process.env.MONGODB)
-.then(
-    () => {
-        console.log("mongoDB connected");
-    }).catch((err) =>{console.log(err)});  
+// mongoose.connect(process.env.MONGODB)
+// .then(
+//     () => {
+//         console.log("mongoDB connected");
+//     }).catch((err) =>{console.log(err)});  
 
-    // mongoose.connect("mongodb://127.0.0.1:27017/my-blog")
-    // .then(()=>{
-    //     console.log("mongodb connected");
-    // })
-    // .catch(()=>{
-    //     console.log("failed to connect to mongodb");
-    // })
+    mongoose.connect("mongodb://127.0.0.1:27017/my-blog")
+    .then(()=>{
+        console.log("mongodb connected");
+    })
+    .catch(()=>{
+        console.log("failed to connect to mongodb");
+    })
 
 
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("server is running on port 3000...");
@@ -32,3 +34,13 @@ app.listen(3000, () => {
 app.use('/user', userRoutes)
 app.use('/auth', authRoutes)
 
+app.use((err,req,res,next) => {
+
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
+});
