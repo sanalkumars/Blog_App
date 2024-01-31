c24dc48c75067dbd2d075347cecfa48b3885bc8d
sanalkumars <sanalskumar126@gmail.com>
2024-01-31 09:43:49 +0530

    created the test api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/userRoute.js'
 
 dotenv.config();
 
@@ -9,10 +10,24 @@ mongoose.connect(process.env.MONGODB)
 .then(
     () => {
         console.log("mongoDB connected");
-    }).catch((err) =>{console.log(err)});
+    }).catch((err) =>{console.log(err)});  
+
+    // mongoose.connect("mongodb://127.0.0.1:27017/my-blog")
+    // .then(()=>{
+    //     console.log("mongodb connected");
+    // })
+    // .catch(()=>{
+    //     console.log("failed to connect to mongodb");
+    // })
+
+
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("server is running on port 3000...");
 });
+
+
+app.use('/user', userRoutes)
+
