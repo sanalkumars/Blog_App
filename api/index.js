cf541eb49067959fcc06bb64ca3bcafb94257a0f
sanalkumars <sanalskumar126@gmail.com>
2024-01-31 10:30:22 +0530

    created signup api routes

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/userRoute.js'
-
+import authRoutes from './routes/authRoute.js'
 dotenv.config();
 
 
@@ -30,4 +30,5 @@ app.listen(3000, () => {
 
 
 app.use('/user', userRoutes)
+app.use('/auth', authRoutes)
 
