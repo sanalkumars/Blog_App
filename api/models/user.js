1678b92e91096e1feadf0c26b952c89b58d1af47
sanalkumars <sanalskumar126@gmail.com>
2024-01-31 09:05:17 +0530

    user model created

---
+++ api/models/user.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+//creating the schema 
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+}, {timestamps:true}
+);
+
+// creating the model
+const User = mongoose.model('User',userSchema);
+
+export default User;
