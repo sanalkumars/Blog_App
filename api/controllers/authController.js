b46f4d0ab22c36418dd61226e336169ac0de2c80
sanalkumars <sanalskumar126@gmail.com>
2024-01-31 11:25:11 +0530

    created the UI for signup page

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -13,7 +13,7 @@ export const signup = async (req,res,next) => {
        next(errorHandler(400,'All fields are required'));
     }
 
-    const hashedPassword =bcryptjs.hashSync(password,10);
+    const hashedPassword = bcryptjs.hashSync(password,10);
 
     const newUser = new User({
         username,
