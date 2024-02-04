ca7fe80b07f2b98f5376a85cf058d9985e3e0e13
sanalkumars <sanalskumar126@gmail.com>
2024-02-04 21:26:48 +0530

    created the api for signin

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/user.js";
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../utils/error.js";
-
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req,res,next) => {
     
@@ -27,8 +27,41 @@ export const signup = async (req,res,next) => {
     } catch (error) {
         next(error);    
     }
+}
 
-    
-    
 
+export const signin = async (req,res,next) =>{
+    const{email,password} =req.body;
+
+    if (!email || !password || email==="" || password==="") {
+        next(errorHandler(400,"All fields must be filled"));
+    }
+
+    try {
+        const validUser =await User.findOne({email});
+        if (!validUser) {
+           return next(errorHandler(404,"User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if (!validPassword) {
+           return next(errorHandler(400,"Wrong Creditinals"))
+        }
+
+        const token = jwt.sign(
+            {id :validUser._id} , process.env.JWT_SECRET,
+            //add expire date if needed {expiry:30d}
+        )
+
+        const{ password: pass, ...rest } = validUser._doc;
+
+         res
+            .status(200)
+             .cookie('access_token',token, {
+               httpOnly:true,
+             })
+             .json(rest);
+
+    } catch (error) {
+        next(error)
+    }
 }
