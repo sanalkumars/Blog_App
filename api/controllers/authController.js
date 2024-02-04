82397af0b17548b24dbc0fbe09266a68ac7237b0
sanalkumars <sanalskumar126@gmail.com>
2024-02-04 21:37:14 +0530

    completed the signin functionality

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -7,7 +7,7 @@ export const signup = async (req,res,next) => {
     
 
     const {username, email, password} = req.body;
- console.log(username,email,password);
+ 
     if ( !username || !email || !password || username==='' || email==='' || password === '' )
      {
        next(errorHandler(400,'All fields are required'));
