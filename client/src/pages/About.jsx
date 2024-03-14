dbc153c1ab35dc392ce2525e62e251bfdc7dcdb0
sanalkumars <sanalskumar126@gmail.com>
2024-03-14 12:20:16 +0530

    project completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div className='max-w-2xl mx-auto p-3 text-center'>
         <div>
           <h1 className='text-3xl font font-semibold text-center my-7'>
-            About Sahand' Blog
+            About My_Blog
           </h1>
           <div className='text-md text-gray-500 flex flex-col gap-6'>
             <p>
